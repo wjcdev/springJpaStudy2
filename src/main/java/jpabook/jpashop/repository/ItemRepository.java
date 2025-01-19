b6d76bcb31923f91176fe78d66525b0ddf4e9eeb
wjcdev <wonjidev@gmail.com>
2025-01-19 23:22:14 +0900

    [상품 도메인 개발] - 상품 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    /**
+     * 상품 저장
+     * @param item
+     */
+    public void save(Item item) {
+        if(item.getId() == null) { // 상품 신규 생성
+            em.persist(item);
+        }else{ // 상품 업데이트와 유사함
+            em.merge(item);
+        }
+    }
+
+    /**
+     * 상품 단건 조회
+     * @param id
+     * @return
+     */
+    public Item findByOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    /**
+     * 상품 리스트 조회
+     * @return
+     */
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
