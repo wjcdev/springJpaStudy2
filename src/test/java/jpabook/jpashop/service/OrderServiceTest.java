e5bacf4bd680939607aea8815a4a0f6325fecbb2
wjcdev <wonjidev@gmail.com>
2025-01-20 03:37:50 +0900

    [주문 도메인 개발] - 주문 기능 테스트

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -9,14 +9,14 @@ import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.OrderRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Fail.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // @ExtendWith : JUnit 4에서 사용되던 @RunWith를 대체
@@ -35,16 +35,9 @@ class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception {
         // given
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "경기", "123-123"));
-        em.persist(member);
+        Member member = getMember();
 
-        Book book = new Book();
-        book.setName("JPA 1");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Book book = getBook("JPA1", 10000, 10);
 
         int orderCount = 2;
 
@@ -60,22 +53,54 @@ class OrderServiceTest {
         assertEquals(8, book.getStockQuantity(), "주문 수량만큼 재고가 줄어야 한다.");
     }
 
-    @Test
-    public void 주문취소() throws Exception {
-        // given
-
-        // when
-
-        // then
+    private Book getBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
     }
+
+    private Member getMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "경기", "123-123"));
+        em.persist(member);
+        return member;
+    }
+
+
 
     @Test
     public void 상품주문_재고수량초과() throws Exception {
-        // given
+        //Given
+        Member member = getMember();
+        Item item = getBook("시골 JPA", 10000, 10);
+        int orderCount = 11; //재고보다 많은 수량
 
-        // when
+        //When
+        orderService.order(member.getId(), item.getId(), orderCount);
 
-        // then
+        //Then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        //Given
+        Member member = getMember();
+        Item item = getBook("시골 JPA", 10000, 10);
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //When
+        orderService.cancelOrder(orderId);
+
+        //Then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals(OrderStatus.CANCEL, getOrder.getStatus(),"주문 취소시 상태는 CANCEL 이다.");
+        assertEquals(10,item.getStockQuantity(),"주문이 취소된 상품은 그만큼 재고가 증가해야 한다.");
     }
     
 }
