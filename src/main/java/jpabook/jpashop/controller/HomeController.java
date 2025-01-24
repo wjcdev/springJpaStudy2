376028805681ea6401cb3875a1130cce4250c1f3
wjcdev <wonjidev@gmail.com>
2025-01-24 22:42:31 +0900

    [웹 계층 개발] - 홈 화면과 레이아웃

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    //Logger log = LoggerFactory.getLogger();
+
+    @RequestMapping("/")
+    public String home() {
+        log.debug("home controller");
+        return "home"; // home.html
+    }
+
+}
